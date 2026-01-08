f4ffd9bfb28e10b60dc79eb7ce9816f8630a9e1f
Youcef Ouadjer <youcefouadjer2016@gmail.com>
2026-01-08 11:36:21 +0100

    checking exceptions

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 # function about the error message inside the file with respect to custom exception
 def error_message_detail(error, error_detail:sys):
@@ -19,4 +19,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by zero ")
+#         raise CustomException
     
