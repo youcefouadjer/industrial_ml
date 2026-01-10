1196d9bd2113527cecbeb66ae0d37606e794c0be
Youcef Ouadjer <youcefouadjer2016@gmail.com>
2026-01-10 23:53:08 +0100

    data ingestion 1st cmt

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,22 @@
-# Python file to handle data ingestio and loading logic from disk or servers
+'''
+Python file to handle data ingestio and loading logic from disk or servers
+'''
+import os
+import sys
+
+from src.exception import CustomException
+from src.logger import logging 
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts', "train.csv")
+    test_data_path: str=os.path.join('artifacts', "test.csv")
+    raw_data_path: str=os.path.join('artifacts', "data.csv")
+    
+
+
